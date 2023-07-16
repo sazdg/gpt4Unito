fad6d9ae68271996b782da712310dc4db03a2314
settimogiacomo <settimogiacomo.98@gmail.com>
2023-07-16 19:12:42 +0200

    Add_webScraping

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
 import os
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
+
 load_dotenv()
 
-doc_reader = PdfReader('documenti/J_K_Rowling_Harry_Potter_e_la_camera_dei_segreti_ita_cap1.pdf')
-#print(doc_reader)
+# Leggi il contenuto del file di testo
+with open('documenti/prova.txt', 'r', encoding='utf-8') as file:
+    raw_text = file.read()
 
-#estrarre da pdf e salvare in variabile
-raw_text = ''
-for i, page in enumerate(doc_reader.pages):
-	text = page.extract_text()
-	if text:
-		raw_text += text
-#print(len(raw_text))
-#print(raw_text[:1000])
-
-
-#split su numero di caratteri
+# Split su numero di caratteri
 text_splitter = CharacterTextSplitter(
-	separator='\n',
-	chunk_size=1000,
-	chunk_overlap=200,
-	length_function=len,
+    separator='\n',
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len,
 )
-texts=text_splitter.split_text(raw_text)
+texts = text_splitter.split_text(raw_text)
 
-print('Numero di chuncks: '+ str(len(texts)))
-#print(texts[0])
+print('Numero di chunks:', len(texts))
 
-#embedding
+# Embedding
 print('--------------------------')
 embedding = OpenAIEmbeddings(openai_api_key=os.getenv('OPENAI_API_KEY'))
 docsearch = FAISS.from_texts(texts, embedding)
 print(docsearch.embedding_function)
 
-query = "Come si chiama la civetta di Harry Potter?"
+query = "Compilazione della Domanda per il conseguimento del titolo di laurea"
 print(query)
 docs = docsearch.similarity_search(query)
 print(docs)
-
--- webscraping.py
+++ webscraping.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+#install lxml
 
-descrizioniLaurea = []
+
 URL = "https://www.didattica-cps.unito.it/do/home.pl/View?doc=Laurearsi/tesi_laurea.html"
 
 page = requests.get(URL)
-soup = BeautifulSoup(page.text, features="html.parser")
+soup = BeautifulSoup(page.text, 'lxml')
+#print(soup.prettify()) #stampa tutto il file html
+title = soup.find('title').get_text()
+transcript = soup.get_text()
+#print(title)
+print(transcript)
 
-# estrarre tutte le occorrenze di un certo tag html
-for p in soup.findAll('div', class_='card'):
-	descrizioniLaurea.append(p)
-	print(p)
-	print("-----------")
+with open('documenti/prova.txt', 'w', encoding='utf-8') as file:
+    file.write(transcript)
+
