66f7837a950a19817c80f06f2dda93b0220fb8ec
settimogiacomo <settimogiacomo.98@gmail.com>
2023-07-26 23:14:00 +0200

    Test domande e risposte

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 load_dotenv()
 argomento = 'tesi_laurea'
@@ -27,7 +29,17 @@ embedding = OpenAIEmbeddings(openai_api_key=os.getenv('OPENAI_API_KEY'))
 docsearch = FAISS.from_texts(texts, embedding)
 print(docsearch.embedding_function)
 
-query = "Come rinunciare all'esame di laurea"
+query = input("Inserisci la domanda:")
 print(query)
 docs = docsearch.similarity_search(query)
-print(docs)
+#print(docs)
+
+chain = load_qa_chain(OpenAI(),chain_type="stuff")
+template_risposta= chain.llm_chain.prompt.template
+#print(template_risposta)
+risposta_finale= chain.run(input_documents=docs,question=query)
+print(risposta_finale)
+
+file_risposta = open("documenti/risposte.txt",'a',encoding='utf-8')
+file_risposta.write("domanda: "+query+"\n"+"risposta: "+risposta_finale+"\n"+"\n")
+file_risposta.close()
--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+#otteniamo solo le informazioni su internet, pulisce il testo
 URL = "https://www.didattica-cps.unito.it/do/home.pl/View?doc=Laurearsi/tesi_laurea.html"
 
 argomento = 'tesi_laurea'
@@ -12,5 +12,8 @@ file = open(f'documenti/{argomento}.txt', 'w', encoding='utf-8')
 for div in soup.findAll('div', class_='card'):
     div = div.get_text().strip().replace(' ', ' ').replace('keyboard_arrow_down', '')
     file.write(div)
+
+
+
 
 
