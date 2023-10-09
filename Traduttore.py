6b2baa2035c8facebf0c4370d3a285f141a52910
settimogiacomo <settimogiacomo.98@gmail.com>
2023-10-09 21:39:05 +0200

    Added class Traduttore

---
+++ Traduttore.py
@@ -0,0 +1,24 @@
+from translate import Translator
+class Traduttore():
+    def __init__(self):
+        self._lingua_input = None
+        self._lingua_output = None
+        self.translator = Translator(to_lang=self._lingua_output, from_lang=self._lingua_input)
+
+    def setLinguaInput(self, linguaInput):
+        self._lingua_input = linguaInput
+
+    def getLinguaInput(self):
+        return self._lingua_input
+
+    def setLinguaOutput(self, linguaOutput):
+        self._lingua_output = linguaOutput
+
+    def getLinguaOutput(self):
+        return self._lingua_output
+
+    def traduci(self, testo):
+        translation = self.translator.translate(testo)
+        return translation
+
+
